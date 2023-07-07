1df8e08667043ec3dd689fa3fd5866ba45687a68
Codec95 <138603471+Codec95@users.noreply.github.com>
2023-07-07 16:52:15 +0200

    Add files via upload

--- Weeks/03.-07.07.23/Test.py
+++ Weeks/03.-07.07.23/Test.py
@@ -1,16 +1,16 @@
-x=0
-y=25
-z=50
-while x<50:
 
-   if x  < y:
-    print('x ist kleiner als y')
-   if x==y:
-    print('x ist jetzt gleich groß wie y')
-   if x > y:
-    print('x ist größer als y')
-   if x==y*2:
-    print('x ist jetzt doppelt so groß wie y')
-   x=x+5
-   print(x)
-print ('Ziel erreicht')
+Buffer = True
+Stack = False
+Ratio = True
+
+
+x = [Buffer, Stack, Ratio]
+
+for v in x:
+	if v:
+		print(v)
+		
+
+print('first value set to', Buffer)
+print('compiling done')
+
