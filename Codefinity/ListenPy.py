5abe90e9ebbe91898a47011ebc23395fb0a855cf
Codec95 <138603471+Codec95@users.noreply.github.com>
2023-07-10 17:37:56 +0200

    Add files via upload

---
+++ Codefinity/ListenPy.py
@@ -0,0 +1,13 @@
+
+
+# Initial list
+people = [["Alex", 23], ["Noah", 34], ["Peter", 29], ["John", 41], ["Michelle", 35]]
+
+# Task 1: information about the fourth person
+print(people[3])
+
+# Task 2: name of the first person
+print(people[0][0])
+
+# Task 3: age of the fifth person
+print(people[4][1])
---
+++ Codefinity/Test.py
@@ -0,0 +1,8 @@
+
+num = 1024
+
+if num != 1024:
+	print('everything should be good!')
+
+else: 
+	print('initial type is', type(num), '/', 'converted float', float(num), '/', 'converted int', int(num), '/', 'converted complex', complex(num))
