add587a33df5d353862f0ff815a8ec7b7923410a
Codec95 <138603471+Codec95@users.noreply.github.com>
2023-07-20 16:27:37 +0200

    Delete Weeks/03.-07.07.23 directory

--- Weeks/03.-07.07.23/forScript.py
+++
@@ -1,9 +0,0 @@
-x = [0,16,32,64,128,256,'Test']
-
-for i in x:
-	print(i)
-	for y in x:
-		print(y)
-
-print('compiling done')
-
--- Weeks/03.-07.07.23/range.py
+++
@@ -1,6 +0,0 @@
-x = [0,16,32,64]
-
-for i in range (0, len(x), 2):
-	print(x[i])
-
-print('compiling done')
