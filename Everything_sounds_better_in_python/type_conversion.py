9e8cab47a6d55590e704301701dd0c1cf04413b2
Codec95 <138603471+Codec95@users.noreply.github.com>
2023-08-07 17:11:08 +0200

    Add files via upload

---
+++ Everything_sounds_better_in_python/type_conversion.py
@@ -0,0 +1,42 @@
+# boolean(bool)
+
+test_bool = True
+is_game_over = False
+
+print(test_bool)
+print(is_game_over)
+
+
+# integer, float, complex(int,float,complex)
+
+num_int = 42
+num_float = 42.22423
+
+print('example for int:', num_int)
+print('example for float:', num_float, 'rounded:', round(num_float, 1))
+
+print(type(num_int))
+print(type(is_game_over))
+
+
+# strings(str)
+
+game_name = 'Valhalla'
+print(game_name)
+
+
+
+# converting
+
+print(int(num_float))
+print(type(num_int), num_int)
+
+num_int_string = str(num_int)
+print(type(num_int_string), num_int_string)
+
+print(bool(0))
+print(bool(1))
+print(bool('Test'))
+print(int(False))
+print(int('42'))
+
---
+++ Everything_sounds_better_in_python/variables.py
@@ -0,0 +1,25 @@
+# boolean(bool)
+
+test_bool = True
+is_game_over = False
+
+print(test_bool)
+print(is_game_over)
+
+
+# integer, float, complex(int,float,complex)
+
+num_int = 42
+num_float = 42.22423
+
+print('example for int:', num_int)
+print('example for float:', num_float, 'rounded:', round(num_float, 1))
+
+print(type(num_int))
+print(type(is_game_over))
+
+
+# strings(str)
+
+game_name = 'Valhalla'
+print(game_name)
