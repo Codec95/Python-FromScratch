ca09525ef9377de715e4f138042eba850e573a0f
DISTRICT_ <138603471+Codec95@users.noreply.github.com>
2024-03-24 10:24:35 +0100

    Delete Codefinity directory

--- Codefinity/Test.py
+++
@@ -1,8 +0,0 @@
-
-num = 1024
-
-if num != 1024:
-	print('everything should be good!')
-
-else: 
-	print('initial type is', type(num), '/', 'converted float', float(num), '/', 'converted int', int(num), '/', 'converted complex', complex(num))
